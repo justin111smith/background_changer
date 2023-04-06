5986e1126141175b72e63fbe66522f56946c71bf
Justin Paul Smith <justin111smith@gmail.com>
2023-04-06 14:21:22 -0600

    Gallery wraps, background opaque, more pictures

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const gallery = document.querySelector('.gallery');
 const body = document.querySelector('body');
+const imageFolder = 'img/'; // Specify the folder path where your images are located
 
 gallery.addEventListener('click', event => {
-  if (event.target.tagName === IMG) {
+  if (event.target.tagName === 'IMG') {
     const imageUrl = event.target.dataset.src;
     body.style.backgroundImage = `url(${imageUrl})`;
+    // Remove the border from all images
+    const images = gallery.querySelectorAll('img');
+    images.forEach(img => (img.style.border = 'none'));
+
+    // Add a border to the clicked image
+    event.target.style.border = '2px solid #fff';
   }
 });
